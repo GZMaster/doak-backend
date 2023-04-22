e616e81572f3cbdc5a273bf691ada1d0966908c9
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-22 23:46:59 +0100

    Revert "payment route set up for you."
    This reverts commit bc4a512266741d47973c85879c0dbe522849e1c6.

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -7,9 +7,5 @@ const router = express.Router();
 // Routes
 router.route("/").post(payment.createPaymentIntent);
 
-router.route("/success").get(payment.executePayment);
-
-router.route("/cancel").get(payment.cancelPayment);
-
 // Export the Express router
 module.exports = router;
