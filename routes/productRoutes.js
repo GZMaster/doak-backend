ab67721bf276a3c41adf976f7feb449b65d68081
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-23 10:50:33 +0100

    setting up user routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,26 +1,7 @@
 const express = require("express");
 const productController = require("../controllers/productController");
-const userController = require("../controllers/userController");
 
 const router = express.Router();
-
-router.post("/signup", userController.signup);
-
-router.post("/login", userController.login);
-
-router.get("/logout", userController.logout);
-
-router.post("/forgotPassword", userController.forgotPassword);
-
-router.patch("/resetPassword/:token", userController.resetPassword);
-
-router.patch(
-  "/updateMyPassword",
-  userController.protect,
-  userController.updatePassword
-);
-
-router.patch("/updateMe", userController.protect, userController.updateMe);
 
 router
   .route("/")
