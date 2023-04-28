343ba09ded678852a0e5dbc6db473074e024336d
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-29 00:03:06 +0100

    userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -111,7 +111,31 @@ exports.signup = catchAsync(async (req, res, next) => {
     otp: otp,
   });
 
-  createSendToken(newUser, 204, res);
+  // createSendToken(newUser, 204, res);
+
+  const token = signToken(newUser._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
+
+  // Remove the password from the output
+  newUser.password = undefined;
+
+  res.status(200).json({
+    status: "success",
+    token,
+    data: {
+      user: newUser,
+    },
+  });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
