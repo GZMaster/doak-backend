a5f3cf1516f9830e9fa235db974e2268c027debb
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-05-06 16:38:24 +0100

    admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/signup", userController.signup);
 
-router.post("/login", userController.restrictTo("admin"), userController.login);
+router.post("/login", userController.login);
 
 router.post(
   "/verifyEmail/:id",
