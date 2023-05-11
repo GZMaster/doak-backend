8b10a7b50f5ef57c01dc9fabba95e471939d0ada
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-05-11 15:31:49 +0100

    setting up payment

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,14 +1,15 @@
 const dotenv = require("dotenv");
+const https = require("https");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const APIFeatures = require("../utils/apiFeatures");
 const Transaction = require("../models/transactionModel");
-const Order = require("../models/orderModel");
+// const Order = require("../models/orderModel");
 
 dotenv.config({ path: "../config.env" });
 
 // eslint-disable-next-line import/no-extraneous-dependencies, import/order, node/no-extraneous-require
-const paystack = require("paystack")(process.env.PAYSTACK_SECRET_KEY);
+// const paystack = require("paystack")(process.env.PAYSTACK_SECRET_KEY);
 
 exports.initializePayment = catchAsync(async (req, res, next) => {
   const { userId, orderId, email, amount } = req.body;
@@ -25,49 +26,69 @@ exports.initializePayment = catchAsync(async (req, res, next) => {
     return next(new AppError("Something went wrong", 400));
   }
 
-  const initTransaction = await paystack.transaction.initialize({
-    amount,
-    email,
+  const params = JSON.stringify({
+    email: req.body.email,
+    amount: req.body.amount,
   });
 
-  if (!initTransaction) {
-    return next(new AppError("Something went wrong", 400));
-  }
+  const options = {
+    hostname: "api.paystack.co",
+    port: 443,
+    path: "/transaction/initialize",
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.PAYSTACK_TESTKEY}`,
+      "Content-Type": "application/json",
+    },
+  };
 
-  // Return the authorization URL to the client
-  res.send(transaction.data.authorization_url);
+  const payReq = https
+    .request(options, (payRes) => {
+      let data = "";
+
+      payRes.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      payRes.on("end", () => {
+        console.log(JSON.parse(data));
+        res.json(JSON.parse(data)); // return the response from Paystack API to the client
+      });
+    })
+    .on("error", (error) => {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while processing your request" }); // handle error and return a response to the client
+    });
+
+  payReq.write(params);
+  payReq.end();
 });
 
 exports.verifyPayment = catchAsync(async (req, res, next) => {
-  const { reference } = req.query;
-
-  const verifyTransaction = await paystack.transaction.verify({ reference });
-
-  if (!verifyTransaction) {
-    return next(new AppError("Something went wrong", 400));
-  }
-
-  const transaction = await Transaction.findOneAndUpdate(
-    { orderId: verifyTransaction.data.metadata.orderId },
-    { paymentStatus: "successful" },
-    { new: true }
-  );
-
-  if (!transaction) {
-    return next(new AppError("Something went wrong", 400));
-  }
-
-  const order = await Order.findOneAndUpdate(
-    { orderId: transaction.orderId },
-    { orderStatus: "paid" },
-    { new: true }
-  );
-
-  if (!order) {
-    return next(new AppError("Something went wrong", 400));
-  }
-
-  res.send("Payment successful");
+  // const { reference } = req.query;
+  // const verifyTransaction = await paystack.transaction.verify({ reference });
+  // if (!verifyTransaction) {
+  //   return next(new AppError("Something went wrong", 400));
+  // }
+  // const transaction = await Transaction.findOneAndUpdate(
+  //   { orderId: verifyTransaction.data.metadata.orderId },
+  //   { paymentStatus: "successful" },
+  //   { new: true }
+  // );
+  // if (!transaction) {
+  //   return next(new AppError("Something went wrong", 400));
+  // }
+  // const order = await Order.findOneAndUpdate(
+  //   { orderId: transaction.orderId },
+  //   { orderStatus: "paid" },
+  //   { new: true }
+  // );
+  // if (!order) {
+  //   return next(new AppError("Something went wrong", 400));
+  // }
+  // res.send("Payment successful");
 });
 
 exports.getAllTransactions = catchAsync(async (req, res, next) => {
